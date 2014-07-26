227daf1be353b46aed7178fff6dfe2d56130f975
Rob Goedman <goedman@mac.com>
2014-07-26 16:33:10 -0700

    Partially switched to common chain format

--- Examples/Bernoulli/bernoulli.jl
+++ Examples/Bernoulli/bernoulli.jl
@@ -11,16 +11,26 @@ data_file = "bernoulli.data.R"
 samples_df = stan(stanmodel, data_file, ProjDir, diagnostics=true)
 
 stan_summary("$(stanmodel.name)_samples_2.csv")
-println("First 5 of $(size(samples_df)[1]) rows of sample_df: ")
-show(samples_df[1:5, :], true)
+res = read_stanfit(stanmodel);
+
+println("$(stanmodel.noofchains) chains: ")
+for i in 1:stanmodel.noofchains
+  res[i] |> display
+  println()
+end
+
+println()
+res[1][:samples] |> display
+println()
+res[1][:diagnostics] |> display
 println()
 
-diags_2_df = read_stanfit("$(stanmodel.name)_diagnostics_2.csv")
+diags_1_df = read_stanfit("$(stanmodel.name)_diagnostics_1.csv")
 
 println()
 using StatsBase
 println("Compare unconstrained diagnostics with sample_df results: \n")
-println([logistic(diags_2_df[1:5, :theta]) samples_df[101:105, :theta]])
+println([logistic(diags_1_df[1:5, :theta]) res[1][:samples][:theta][1:5]])
 println()
 
 cd(old)
