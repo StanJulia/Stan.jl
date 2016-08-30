b3fb31c80e127121bb015669a9b763fb1b20393b
Rob J Goedman <goedman@mac.com>
2016-08-30 11:29:10 -0700

    Updated build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -3,11 +3,13 @@ using BinDeps
 @BinDeps.setup
 cmdstan = library_dependency("cmdstan")
 
-@static is_apple() begin
-    if Pkg.installed("Homebrew") === nothing
-        error("Homebrew package not installed, please run Pkg.add(\"Homebrew\")")
-    end
-    
-    using Homebrew
-    provides( Homebrew.HB, "homebrew/science/cmdstan", cmdstan, os = :Darwin )
-end
+function install_cmdstan()
+  if Pkg.installed("Homebrew") === nothing
+      error("Homebrew package not installed, please run Pkg.add(\"Homebrew\")")
+  end
+  
+  using Homebrew
+  provides( Homebrew.HB, "homebrew/science/cmdstan", cmdstan, os = :Darwin )
+end  
+  
+@static is_apple() ? install_cmdstan() : Nothing
