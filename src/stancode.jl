c7086d44e894a591d5054d42b2cac2ce48bd953f
Jon Alm Eriksen <jon.alm.eriksen@gmail.com>
2016-10-17 12:43:39 +0200

    significanly speedup update_R_file by writing pushing to array instead of growing string

--- src/stancode.jl
+++ src/stancode.jl
@@ -132,6 +132,49 @@ function stan(
 end
 
 function update_R_file{T<:Any}(file::String, dct::Dict{String, T}; replaceNaNs::Bool=true)
+    isfile(file) && rm(file)
+    strmout = open(file, "w")
+  
+    for entry in dct
+        line = Any["\"", entry[1], "\"", " <- "]
+        val = entry[2]
+        if length(val)==1 && length(size(val))==0
+            # Scalar
+            push!(line,val)
+        elseif length(val)==1 && length(size(val))==1
+            # Single element vector
+            push!(line,val[1])
+        elseif length(val)>1 && length(size(val))==1
+            # Vector
+            push!(line, "structure(c(")
+            for i in 1:length(val)
+                push!(line,val[i])
+                if i < length(val)
+                    push!(line,", ")
+                end
+            end
+            push!(line,"), .Dim=c(")
+            push!(line,length(val))
+            push!(line,"))")
+        elseif length(val)>1 && length(size(val))>1
+            # Array             
+            push!(line,"structure(c(" )
+            for i in 1:length(val)
+                push!(line,val[i])              
+                if i < length(val)
+                    push!(line,", ")
+                end
+            end
+            push!(line, "), .Dim=c")
+            push!(line, size(val))
+            push!(line, ")")
+        end
+        println(strmout, line...)
+    end
+    close(strmout)
+end
+
+function update_R_file_old{T<:Any}(file::String, dct::Dict{String, T}; replaceNaNs::Bool=true)
   isfile(file) && rm(file)
   strmout = open(file, "w")
   
@@ -487,3 +530,4 @@ function cmdline(m)
   end
   cmd
 end
+
