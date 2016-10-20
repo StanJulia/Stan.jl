8d3310b5b209737d4220c1f25bdfd0de77177590
Rob J Goedman <goedman@mac.com>
2016-10-20 12:59:01 -0700

    Made sure testdataxxx is in test directory and deleted.

--- test/test_csv.jl
+++ test/test_csv.jl
@@ -16,8 +16,17 @@ data = Dict(
   "Array" => rand(N,3)
 )
 
-Stan.update_R_file("testdata0", data1)
-Stan.update_R_file_jon("testdata1", data1)
+ProjDir = dirname(@__FILE__)
+cd(ProjDir) do
 
-@time Stan.update_R_file("testdata0", data)
-@time Stan.update_R_file_jon("testdata1", data)
+  Stan.update_R_file("testdata0", data1)
+  Stan.update_R_file_jon("testdata1", data1)
+
+  @time Stan.update_R_file("testdata0", data)
+  @time Stan.update_R_file_jon("testdata1", data)
+
+  isfile("testdata0") && rm("testdata0");
+  isfile("testdata1") && rm("testdata1");
+
+end # cd
+
