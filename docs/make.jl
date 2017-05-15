804158090cd8fcc5899471ce36e3b5232c281748
Rob J Goedman <goedman@mac.com>
2017-05-15 12:59:11 -0700

    Updated doc/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -13,7 +13,7 @@ makedocs(
 )
 
 deploydocs(
-    repo = "github.com/goedman/Stan.jl,git",
+    repo = "github.com/goedman/Stan.jl.git",
     target = "build",
     julia = "0.6",
     osname = "linux",
