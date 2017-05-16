f85f1dc716e42431c0de03c62d47f1bdda17c8c2
autodocs <autodocs>
2017-05-16 15:25:16 +0000

    build based on 7019f8c

--- latest/search_index.js
+++ latest/search_index.js
@@ -82,15 +82,15 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "WALKTHROUGH.html#",
-    "page": "Example walkthrough",
-    "title": "Example walkthrough",
+    "page": "Walkthrough",
+    "title": "Walkthrough",
     "category": "page",
     "text": ""
 },
 
 {
     "location": "WALKTHROUGH.html#A-walk-through-example-1",
-    "page": "Example walkthrough",
+    "page": "Walkthrough",
     "title": "A walk-through example",
     "category": "section",
     "text": ""
@@ -98,15 +98,15 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "WALKTHROUGH.html#Bernoulli-example-1",
-    "page": "Example walkthrough",
+    "page": "Walkthrough",
     "title": "Bernoulli example",
     "category": "section",
-    "text": "In this walk-through, it is assumed that 'ProjDir' holds a path to where transient files will be created (in a subdirectory /tmp of ProjDir).Make Stan.jl and Mamba diagnostics and graphics available:using Mamba, StanNext define the variable 'bernoullistanmodel' to hold the Stan model definition:const bernoullistanmodel = \"\ndata { \n  int<lower=0> N; \n  int<lower=0,upper=1> y[N];\n} \nparameters {\n  real<lower=0,upper=1> theta;\n} \nmodel {\n  theta ~ beta(1,1);\n    y ~ bernoulli(theta);\n}\n\"The next step is to create a Stanmodel object. The most common way to create such an object is by giving the model a name while the Stan model is passed in, both through keyword (hence optional) arguments:stanmodel = Stanmodel(name=\"bernoulli\", model=bernoullistanmodel);\nstanmodel |> displayAbove Stanmodel() call creates a default model for sampling. Other arguments to Stanmodel() can be found in StanmodelThe onserved input data is defined below.const bernoullidata = Dict(\"N\" => 10, \"y\" => [0, 1, 0, 1, 0, 0, 0, 0, 0, 1])Run the simulation by calling stan(), passing in the data and the intended working directory. rc, sim1 = stan(stanmodel, bernoullidata, ProjDir, CmdStanDir=CMDSTAN_HOME)More documentation on stan() can be found in stanIf the return code rc indicated success (rc == 0), Mamba.jl provides the describe() function. We can't use all monitored variables by Stan. In this example a good subset is selected as below and stored in 'sim'if rc == 0\n  sim1 = stan(stanmodel, [bernoullidata], ProjDir, CmdStanDir=CMDSTAN_HOME)\n  println(\"Subset Sampler Output\")\n  sim = sim1[1:1000, [\"lp__\", \"theta\", \"accept_stat__\"], :]\n  describe(sim)\nendThe first time (or when updates to the model or data have been made) stan() will compile the model and create the executable.On Windows, the CmdStanDir argument appears needed (this is still being investigated). On OSX/Unix CmdStanDir is obtained from either ~/.juliarc.jl or an environment variable (see the Requirements section).By default stan() will run 4 chains, optionally display a combined summary and returns a Mamba Chains object for a sampler. Some other Stan methods, e.g. optimize, return a dictionary.The following diagnostics and Gadfly based plot functions (all from Mamba.jl) are available:println(\"Brooks, Gelman and Rubin Convergence Diagnostic\")\ntry\n  gelmandiag(sim, mpsrf=true, transform=true) |> display\ncatch e\n  #println(e)\n  gelmandiag(sim, mpsrf=false, transform=true) |> display\nend\nprintln()\n\nprintln(\"Geweke Convergence Diagnostic\")\ngewekediag(sim) |> display\nprintln()\n\nprintln(\"Highest Posterior Density Intervals\")\nhpd(sim) |> display\nprintln()\n\nprintln(\"Cross-Correlations\")\ncor(sim) |> display\nprintln()\n\nprintln(\"Lag-Autocorrelations\")\nautocor(sim) |> display\nprintln()To plot the simulation results:p = plot(sim, [:trace, :mean, :density, :autocor], legend=true);\ndraw(p, ncol=4, filename=\"summaryplot\", fmt=:svg)\ndraw(p, ncol=4, filename=\"summaryplot\", fmt=:pdf)"
+    "text": "In this walk-through, it is assumed that 'ProjDir' holds a path to where transient files will be created (in a subdirectory /tmp of ProjDir).Make Stan.jl and Mamba diagnostics and graphics available:using Mamba, StanNext define the variable 'bernoullistanmodel' to hold the Stan model definition:const bernoullistanmodel = \"\ndata { \n  int<lower=0> N; \n  int<lower=0,upper=1> y[N];\n} \nparameters {\n  real<lower=0,upper=1> theta;\n} \nmodel {\n  theta ~ beta(1,1);\n    y ~ bernoulli(theta);\n}\n\"The next step is to create a Stanmodel object. The most common way to create such an object is by giving the model a name while the Stan model is passed in, both through keyword (hence optional) arguments:stanmodel = Stanmodel(name=\"bernoulli\", model=bernoullistanmodel);\nstanmodel |> displayAbove Stanmodel() call creates a default model for sampling. Other arguments to Stanmodel() can be found in StanmodelThe observed input data is defined below.const bernoullidata = Dict(\"N\" => 10, \"y\" => [0, 1, 0, 1, 0, 0, 0, 0, 0, 1])Run the simulation by calling stan(), passing in the data and the intended working directory. rc, sim1 = stan(stanmodel, bernoullidata, ProjDir, CmdStanDir=CMDSTAN_HOME)More documentation on stan() can be found in stanIf the return code rc indicated success (rc == 0), Mamba.jl provides the describe() function. We can't use all monitored variables by Stan. In this example a good subset is selected as below and stored in 'sim'if rc == 0\n  sim1 = stan(stanmodel, [bernoullidata], ProjDir, CmdStanDir=CMDSTAN_HOME)\n  println(\"Subset Sampler Output\")\n  sim = sim1[1:1000, [\"lp__\", \"theta\", \"accept_stat__\"], :]\n  describe(sim)\nendThe first time (or when updates to the model have been made) stan() will compile the model and create the executable.On Windows, the CmdStanDir argument appears needed (this is still being investigated). On OSX/Unix CmdStanDir is obtained from either ~/.juliarc.jl or an environment variable (see the Requirements section).By default stan() will run 4 chains, optionally display a combined summary and returns a Mamba Chains object for a sampler. Some other Stan methods, e.g. optimize, return a dictionary.The following diagnostics and Gadfly based plot functions (all from Mamba.jl) are available:println(\"Brooks, Gelman and Rubin Convergence Diagnostic\")\ntry\n  gelmandiag(sim, mpsrf=true, transform=true) |> display\ncatch e\n  #println(e)\n  gelmandiag(sim, mpsrf=false, transform=true) |> display\nend\nprintln()\n\nprintln(\"Geweke Convergence Diagnostic\")\ngewekediag(sim) |> display\nprintln()\n\nprintln(\"Highest Posterior Density Intervals\")\nhpd(sim) |> display\nprintln()\n\nprintln(\"Cross-Correlations\")\ncor(sim) |> display\nprintln()\n\nprintln(\"Lag-Autocorrelations\")\nautocor(sim) |> display\nprintln()To plot the simulation results:p = plot(sim, [:trace, :mean, :density, :autocor], legend=true);\ndraw(p, ncol=4, filename=\"summaryplot\", fmt=:svg)\ndraw(p, ncol=4, filename=\"summaryplot\", fmt=:pdf)"
 },
 
 {
     "location": "WALKTHROUGH.html#Running-a-Stan-script,-some-details-1",
-    "page": "Example walkthrough",
+    "page": "Walkthrough",
     "title": "Running a Stan script, some details",
     "category": "section",
     "text": "Stan.jl really only consists of 2 functions, Stanmodel() and stan()."
@@ -114,18 +114,18 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "WALKTHROUGH.html#[Stanmodel](@ref)-1",
-    "page": "Example walkthrough",
+    "page": "Walkthrough",
     "title": "Stanmodel",
     "category": "section",
-    "text": "Stanmodel() is used to define the basic attributes for a model:monitor = [\"theta\", \"lp__\", \"accept_stat__\"]\nstanmodel = Stanmodel(name=\"bernoulli\", model=bernoulli, monitors=monitor);\nstanmodelShows all parameters in the model, in this case (by default) a sample model.Compared to the call to Stanmodel() above, the keyword argument monitors has been added. This means that after the simulation is complete, only the monitored variables will be read in from the .csv file produced by Stan. This can be useful if many, e.g. 100s, nodes are being observed.stanmodel2 = Stanmodel(Sample(adapt=Adapt(delta=0.9)), name=\"bernoulli2\", nchains=6)An example of updating default model values when creating a model. The format is slightly different from CmdStan, but the parameters are as described in the CmdStan Interface User's Guide. This is also the case for the Stanmodel() optional arguments random, init and output (refresh only).Now stanmodel2 will look like:stanmodel2After the Stanmodel object has been created fields can be updated, e.g.stanmodel2.method.adapt.delta=0.85"
+    "text": "Stanmodel() is used to define the basic attributes for a model:monitor = [\"theta\", \"lp__\", \"accept_stat__\"]\nstanmodel = Stanmodel(name=\"bernoulli\", model=bernoulli, monitors=monitor);\nstanmodelAbove script, in the Julia REPL, shows all parameters in the model, in this case (by default) a sample model.Compared to the call to Stanmodel() above, the keyword argument monitors has been added. This means that after the simulation is complete, only the monitored variables will be read in from the .csv file produced by Stan. This can be useful if many, e.g. 100s, nodes are being observed.stanmodel2 = Stanmodel(Sample(adapt=Adapt(delta=0.9)), name=\"bernoulli2\", nchains=6)An example of updating default model values when creating a model. The format is slightly different from CmdStan, but the parameters are as described in the CmdStan Interface User's Guide. This is also the case for the Stanmodel() optional arguments random, init and output (refresh only).Now, in the REPL, the stanmodel2 can be shown by:stanmodel2After the Stanmodel object has been created fields can be updated, e.g.stanmodel2.method.adapt.delta=0.85"
 },
 
 {
     "location": "WALKTHROUGH.html#[stan](@ref)-1",
-    "page": "Example walkthrough",
+    "page": "Walkthrough",
     "title": "stan",
     "category": "section",
-    "text": "After a Stanmodel has been created, the workhorse function stan() is called to run the simulation.After the stan() call, the stanmodel.command contains an array of Cmd fields that contain the actual run commands for each chain. These are executed in parallel. The call to stan() might update other info in the StanModel, e.g. the names of diagnostics files.The stan() call uses 'make' to create (or update when needed) an executable with the given model.name, e.g. bernoulli in the above example. If no model String (or of zero length) is found, a message will be shown."
+    "text": "After a Stanmodel has been created, the workhorse function stan() is called to run the simulation. Note that some fields in the Stanmodel are updated by stan().After the stan() call, the stanmodel.command contains an array of Cmd fields that contain the actual run commands for each chain. These are executed in parallel. The call to stan() might update other info in the StanModel, e.g. the names of diagnostics files.The stan() call uses 'make' to create (or update when needed) an executable with the given model.name, e.g. bernoulli in the above example. If no model String (or of zero length) is found, a message will be shown."
 },
 
 {
