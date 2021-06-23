65735f8d25931b3f5ebad85bcdd3131debf34fd9
Rob J Goedman <goedman@icloud.com>
2021-06-23 07:58:27 -0600

    Rel 6.5.0

--- Examples/Bernoulli/bernoulli2.jl
+++ Examples/Bernoulli/bernoulli2.jl
@@ -6,8 +6,8 @@ ProjDir = @__DIR__
 
 bernoullimodel = "
 data { 
-  int<lower=1> N; 
-  int<lower=0,upper=1> y[N];
+  int<lower=0> N; 
+  array[N] int<lower=0,upper=1> y;
 } 
 parameters {
   real<lower=0,upper=1> theta;
