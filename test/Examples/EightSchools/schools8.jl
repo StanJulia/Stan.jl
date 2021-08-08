8998c0ff3db4aaeb071e744c533c786df87d6e27
Rob J Goedman <goedman@icloud.com>
2021-08-08 11:09:54 -0600

    Rel 7.0.1 - Schools8 to KeyedArray chains

--- test/Examples/EightSchools/schools8.jl
+++ test/Examples/EightSchools/schools8.jl
@@ -35,14 +35,29 @@ sm = SampleModel("schools8", eightschools)
 rc = stan_sample(sm, data=schools8data)
 
 if success(rc)
-  samples = read_samples(sm)
-  
-  df = read_summary(sm)
-  @test df[df.parameters .== :mu, :mean][1] ≈ 7.64 rtol=0.5
-  @test df[df.parameters .== :tau, :mean][1] ≈ 6.50 rtol=0.5
-  @test df[df.parameters .== Symbol("theta[1]"), :mean][1] ≈ 11.1 rtol=0.5
-  @test df[df.parameters .== Symbol("theta[8]"), :mean][1] ≈ 8.3 rtol=0.5
-  @test df[df.parameters .== Symbol("eta[1]"), :mean][1] ≈ 0.4 rtol=0.5
-  @test df[df.parameters .== Symbol("eta[8]"), :mean][1] ≈ 0.066 rtol=0.5
+
+    chns = read_samples(sm)
+    chns |> display
+    println()
+
+    chns_eta = matrix(chns, :eta)
+    chns_eta |> display
+    println()
+    
+    ndraws, nchains, nparams = size(chns_eta)
+    chn_eta = reshape(chns_eta, ndraws*nchains, nparams)
+    means = mean(chn_eta, dims=1)
+    means |> display
+    println()
+
+    df = read_summary(sm)
+    @test df[df.parameters .== :mu, :mean][1] ≈ 7.64 rtol=0.5
+    @test df[df.parameters .== :tau, :mean][1] ≈ 6.50 rtol=0.5
+    @test df[df.parameters .== Symbol("theta[1]"), :mean][1] ≈ 11.1 rtol=0.5
+    @test df[df.parameters .== Symbol("theta[8]"), :mean][1] ≈ 8.3 rtol=0.5
+    @test df[df.parameters .== Symbol("eta[1]"), :mean][1] ≈ 0.4 rtol=0.5
+    @test df[df.parameters .== Symbol("eta[8]"), :mean][1] ≈ 0.066 atol=0.5
+
+    @test means ≈ [0.4  0.009  -0.22  -0.04  -0.4  -0.2  0.3  0.07] atol=0.5
 
 end
