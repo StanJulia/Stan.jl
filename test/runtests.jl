d3c3630f02f605e7e28f43385bf092a0d6dc345c
Rob J Goedman <goedman@icloud.com>
2022-01-21 12:28:10 -0700

    Rel 8.4.0 - Updating example tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,7 +6,7 @@ TestDir = @__DIR__
 ExampleDir = joinpath(TestDir, "..")
 
 examples = [
-  
+
   "../Examples/ARM/Ch03/Kid/kidscore.jl",
   "../Examples/Bernoulli/bernoulli.jl",
   "../Examples/Bernoulli/bernoulli2.jl",
@@ -25,17 +25,17 @@ examples = [
   "../Examples/WaffleDivorce/waffle_divorce_keyedarray.jl",
 
   "../Examples/Walkthrough/walkthrough2.jl",
-
-  "../Examples_Stan_Methods/Diagnose/bernoulli_diagnose.jl",
-  "../Examples_Stan_Methods/Optimize/bernoulli_optimize.jl",
+  "../Examples_Stan_Methods/Diagnose/diagnose.jl",
+  "../Examples_Stan_Methods/Optimize/optimize.jl",
   "../Examples_Stan_Methods/Generate_Quantities/generate_quantities.jl",
-  "../Examples_Stan_Methods/Parse_and_Interpolate/example.jl",
 
-  #"../Examples/Variational/variational.jl",
-  "../Examples/StanQuap/howell1.jl",
+  "../Examples_Stan_Methods/Parse_and_Interpolate/parse.jl",
+
+  "../Examples_Stan_Methods/Variational/variational.jl",
+  "../Examples_Stan_Methods/StanQuap/howell1.jl",
 
   "../Examples_Test_Cases/Diagnostics/diagnostics.jl",
-  "../Examples_Test_Cases/InitThetaDict/init_dict.jl",
+  "../Examples_Test_Cases/InitThetaDict/init_theta.jl",
   "../Examples_Test_Cases/InitThetaDictArray/init_dict_array.jl",
   "../Examples_Test_Cases/InitThetaFile/init_file.jl",
   "../Examples_Test_Cases/ScalarObs/scalar.jl",
@@ -43,10 +43,8 @@ examples = [
 
   "Examples/ARM/Ch03/Kid/kidscore.jl",
   "Examples/Bernoulli/bernoulli.jl",
-  "Examples/Bernoulli/bernoulli2.jl",
   "Examples/Binomial/binomial.jl",
   "Examples/Binormal/binormal.jl",
-  "Examples/Diagnose/diagnose.jl",
   "Examples/Dyes/dyes.jl",
   "Examples/EightSchools/schools8.jl",
 
