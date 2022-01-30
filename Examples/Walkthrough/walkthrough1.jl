42790fd0d7b8a8537290510124c36a2c5ebbf85a
Rob J Goedman <goedman@icloud.com>
2022-01-30 15:40:18 -0700

    Rel 9.0.1 - Updated README on C++ enabling in cmdstan

--- Examples/Walkthrough/walkthrough1.jl
+++ Examples/Walkthrough/walkthrough1.jl
@@ -18,7 +18,7 @@ sm = SampleModel("bernoulli", model);
 
 data = Dict("N" => 10, "y" => [0, 1, 0, 1, 0, 0, 0, 0, 0, 1]);
 
-rc = stan_sample(sm; data);
+rc = stan_sample(sm; num_chains=4, data);
 
 if success(rc)
   df = read_samples(sm, :dataframe);
