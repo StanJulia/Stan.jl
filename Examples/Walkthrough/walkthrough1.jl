14f69925fb3ea278dbaf0fda61b40fcb814a7ba4
Rob J Goedman <goedman@icloud.com>
2022-01-30 13:48:52 -0700

    Rel 9.0.1 - Added walkthrough1.jl

---
+++ Examples/Walkthrough/walkthrough1.jl
@@ -0,0 +1,26 @@
+using StanSample, DataFrames
+
+model = "
+data { 
+  int<lower=0> N; 
+  int<lower=0,upper=1> y[N];
+} 
+parameters {
+  real<lower=0,upper=1> theta;
+} 
+model {
+  theta ~ beta(1,1);
+    y ~ bernoulli(theta);
+}
+";
+
+sm = SampleModel("bernoulli", model);
+
+data = Dict("N" => 10, "y" => [0, 1, 0, 1, 0, 0, 0, 0, 0, 1]);
+
+rc = stan_sample(sm; data);
+
+if success(rc)
+  df = read_samples(sm, :dataframe);
+  df |> display
+end
