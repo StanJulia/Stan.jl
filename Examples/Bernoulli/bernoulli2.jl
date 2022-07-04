ef67ecafb0b821d88851b603d6a63e2946260430
Rob J Goedman <goedman@icloud.com>
2022-07-04 09:36:58 -0600

    Rel 9.8.2 - Switch to cmdstan-2.30.0

--- Examples/Bernoulli/bernoulli2.jl
+++ Examples/Bernoulli/bernoulli2.jl
@@ -7,7 +7,7 @@ ProjDir = @__DIR__
 bernoullimodel = "
 data { 
   int<lower=0> N; 
-  array[N] int<lower=0,upper=1> y;
+  array[N] int<lower=0, upper=1> y;
 } 
 parameters {
   real<lower=0,upper=1> theta;
